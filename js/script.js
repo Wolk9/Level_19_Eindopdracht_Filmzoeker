6d7d84a5abadac3efaa4d2d6fcedb318b0d81733
Martin de Bes on MacBook Air <martin.de.bes@me.com>
2021-05-31 15:00:18 +0200

    prutsen

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log(movies);
+// console.log(movies);
+
+// let btnGet = document.querySelector("label, value");
+// let input = document.querySelector("input");
+// let result = document.querySelector("film-list");
+
+// btnGet.addEventListener("click", () => {
+//   let value = input.value;
+//   result.innerHTML = Object.keys(movies).find((key) => movies[key] == value);
+//   console.log(value);
+// });
+
+let keys = Object.keys(movies);
+//console.log("keys:", keys);
+let vals = Object.values(movies);
+// console.log("values:", vals);
+let entries = Object.entries(movies);
+//console.log("entries:", entries);
+
+//movies.forEach((number, index, array) => console.log(movies[4].Title));
+
+let selection = (selected) => {
+  for (i = 0; i < movies.length; i++) {}
+};
